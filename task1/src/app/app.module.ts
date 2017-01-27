211866a457dc9d08b837c13f76ee8e8a5fd00492
noctua <noctuarox@seznam.cz>
2017-01-27 17:56:10 +0100

    feat(thumbnail): Added thumbnail component.

--- task1/src/app/app.module.ts
+++ task1/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { ThumbnailComponent } from './thumbnail/thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ThumbnailComponent
   ],
   imports: [
     BrowserModule,
